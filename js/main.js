7ea23d0efb0b7808adaa860043355182009c54bf
Sora Arakawa <sr@arkw.jp>
2021-07-24 23:16:14 +0900

    ページ読み込み速度を改善

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
-var pmid = "windows";
+let pmid = "windows";
+const slideShow = [
+	{
+		Image: "md_prohaya",
+		URL: "https://arkw.net/products/web/prohaya/"
+	},
+	{
+		Image: "md_dokaben",
+		URL: "https://arkw.net/products/web/dokaben/"
+	},
+	{
+		Image: "md_psk",
+		URL: "https://psk.arkw.net/"
+	},
+	{
+		Image: "md_kin",
+		URL: "https://arkw.net/products/web/kintetsu/"
+	},
+	{
+		Image: "md_mei",
+		URL: "https://arkw.net/products/web/meitetsu/"
+	},
+];
 
-$(document).ready(function(){
-	$('.slider').slick({
+window.onload = function() {
+	createSlideShow();
+	$(".slider").slick({
 		autoplay: true,
 		arrows: false
 	});
-});
+	$("body").css("font-family", "Noto Sans JP");
+}
 
 function changeProductsMenu(a){
 	if(pmid != a){
@@ -15,8 +39,18 @@ function changeProductsMenu(a){
 		$("#pr_item_windows").hide(100);
 		$("#pr_item_web").hide(100);
 		$("#pr_item_dev").hide(100);
-		$("#pr_menu_"+a).addClass("pr_menu_selected");
-		$("#pr_item_"+a).show(100);
+		$("#pr_menu_" + a).addClass("pr_menu_selected");
+		$("#pr_item_" + a).show(100);
 		pmid = a;
 	}
+}
+
+function createSlideShow() {
+	let output = "";
+	output += '<div class="slider">';
+	for (let i = 0; i < slideShow.length; i++) {
+		output += '<a href="' + slideShow[i].URL + '" target="blank"><div><img src="img/header/' + slideShow[i].Image + '.png"></div></a>';
+	}
+	output += '</div>';
+	$(".navi").html(output);
 }
