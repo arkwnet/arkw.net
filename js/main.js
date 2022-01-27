0ab7066d8d51f6b99f90b8931c110d80cd3cc874
Sora Arakawa <sr@arkw.jp>
2022-01-27 16:51:47 +0900

    トップページの更新情報を動的生成に変更

--- js/main.js
+++ js/main.js
@@ -28,24 +28,11 @@ const slideShow = [
 
 window.onload = function() {
 	createSlideShow();
+	createUpdateInformation();
 	$(".slider").slick({
 		autoplay: true,
 		arrows: false
 	});
-}
-
-function changeProductsMenu(a){
-	if(pmid != a){
-		$("#pr_menu_windows").removeClass("pr_menu_selected");
-		$("#pr_menu_web").removeClass("pr_menu_selected");
-		$("#pr_menu_dev").removeClass("pr_menu_selected");
-		$("#pr_item_windows").hide(100);
-		$("#pr_item_web").hide(100);
-		$("#pr_item_dev").hide(100);
-		$("#pr_menu_" + a).addClass("pr_menu_selected");
-		$("#pr_item_" + a).show(100);
-		pmid = a;
-	}
 }
 
 function createSlideShow() {
@@ -56,4 +43,32 @@ function createSlideShow() {
 	}
 	output += '</div>';
 	$(".navi").html(output);
+}
+
+function createUpdateInformation() {
+	let output = "";
+	output += "<table>";
+	for (let i = 0; i < 5; i++) {
+		output += "<tr><th>" + history[i].Date + "</th><td>";
+		for (let j = 0; j < history[i].Content.length; j++) {
+			output += history[i].Content[j] + "<br>";
+		}
+		output += "</td></tr>"
+	}
+	output += "</table>";
+	$("#update_information").html(output);
+}
+
+function changeProductsMenu(a){
+	if (pmid != a) {
+		$("#pr_menu_windows").removeClass("pr_menu_selected");
+		$("#pr_menu_web").removeClass("pr_menu_selected");
+		$("#pr_menu_dev").removeClass("pr_menu_selected");
+		$("#pr_item_windows").hide(100);
+		$("#pr_item_web").hide(100);
+		$("#pr_item_dev").hide(100);
+		$("#pr_menu_" + a).addClass("pr_menu_selected");
+		$("#pr_item_" + a).show(100);
+		pmid = a;
+	}
 }
